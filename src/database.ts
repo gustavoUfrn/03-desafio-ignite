78a05c473d19317eaf449380f708049f3a6dc2d8
Gustavo <mxgugaa@gmail,com>
2024-03-18 22:01:57 -0300

    c2

--- src/database.ts
+++ src/database.ts
@@ -8,7 +8,7 @@ export const config: Knex.Config = {
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
-    directory: "./migrations",
+    directory: "./tmp/db/migrations",
   },
 };
 
